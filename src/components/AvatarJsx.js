7863acf47ca85b08e6756610782ffa032214449b
Jgonzalez <Jgonzalez@DESARROLLO2>
2022-09-20 11:25:30 -0300

    creación de mi app

---
+++ src/components/AvatarJsx.js
@@ -0,0 +1,10 @@
+const AvatarJsx = (params) => {
+    const src = 'https://i.pinimg.com/originals/80/57/b8/8057b84fe3a3da44bdaeffc78a8da1b4.jpg'
+    return(
+        <picture>
+            <img src={src} alt='vans-logo' />
+        </picture>
+    )
+}
+
+export default AvatarJsx
