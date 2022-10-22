4f08d71df18912976db29fdb5e8f82b12f58290a
Juan Gonzalez <juangonzalez.coderhouse@gmail.com>
2022-10-22 18:52:08 -0300

    Se añadió el componente Notification.

---
+++ src/notification/notification.jsx
@@ -0,0 +1,34 @@
+import './notification.css'
+import { createContext, useState } from "react"
+
+const Notification = ({msg, severity}) => {
+    if(msg === '') return
+
+    return (
+      <div className={severity === 'success' ? 'div-Notification success' : 'div-Notification error'}>
+        {msg}
+      </div>
+    )
+}
+
+const NotificationContext = createContext()
+
+export const NotificationProvider = ({children}) => {
+    const [message, setMessage] = useState('')
+    const [severity, setSeverity] = useState('success')
+
+    const setNotification = (severity, msg) => {
+        setMessage(msg)
+        setSeverity(severity)
+        setTimeout(() => {
+            setMessage('')
+        }, 3000)
+    }
+
+    return (
+        <NotificationContext.Provider value={{setNotification}}>
+            <Notification msg={message} svt={severity}/>
+            {children}
+        </NotificationContext.Provider>
+    )
+}
